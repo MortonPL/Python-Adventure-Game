57c7656037cb0d711105e97871aa5c172ed0b8b7
MortonPL <MortonPL@users.noreply.github.com>
2019-12-23 02:00:03 +0100

    Unstable version, fix printing

--- main.py
+++ main.py
@@ -8,8 +8,15 @@ class Game:
 
     def __init__(self):
         self.screens = {'menu': Screen('menu')}
-        menu = Menu(self, "mainmenu.txt")
-        self.clear()
+        self.rollback = None
+        menu_main = Menu(self, "mainmenu.txt", 1, False,
+                         {"1": "newgame",
+                          "2": "loadgame",
+                          "3": "exit"})
+        menu_intro = Menu(self, "mainmenu.txt", 2, True,
+                          {"any": "exit"})
+        self.menu_list = [menu_intro, menu_intro]
+        menu_main.start()
 
     def screen_get(self, key):
         return self.screens[key]
@@ -19,29 +26,82 @@ class Game:
 
     def screen_refresh(self, key):
         self.clear()
-        self.screens[key].show()
+        self.screen_get(key).show()
 
     def screen_reset(self, key):
-        self.screens[key].clear_memory()
+        self.screen_get(key).clear_memory()
 
     def clear(self):
         os.system('cls')
+        os.system('cls')
 
-    def exit(self, roll_function):
+    def update_rollback(self, function):
+        self.rollback = function
+
+    def exit(self):
         if input('\nAre you sure about that? (Type in QUIT to confirm.) ') == \
                 'QUIT':
+            self.clear
             exit()
         else:
-            self.rollback(roll_function)
+            print("we're back")
+            self.rollback()
 
-    def rollback(self, roll_function):
-        print("we're back")
-        roll_function()
-
-    def question(self, key):
-        self.screens[key].add('\nWhat do you do? ', True)
-        self.screen_refresh(key)
+    def question(self, key, mute_question):
+        if not mute_question:
+            self.screen_get(key).add('\nWhat do you do? ', True)
+            self.screen_refresh(key)
         return input()
+
+
+class Menu:
+
+    def __init__(self, game, menufile_path, file_section, any_key, choices):
+        self.game = game
+        self.menufile = ReaderTXT(menufile_path)
+        self.file_section = file_section
+        self.any_key = any_key
+        self.choices = choices
+
+    def start(self):
+        self.game.clear()
+        self.read_menu()
+        self.userchoice()
+
+    def read_menu(self):
+        self.game.screen_reset('menu')
+        for line in self.menufile.get_section(self.file_section):
+            self.game.screen_get('menu').add(line)
+        self.game.screen_refresh('menu')
+
+    def userchoice(self):
+        self.game.update_rollback(self.userchoice)
+        answer = self.game.question('menu', False)
+        valid_answer = False
+        if self.any_key:
+            valid_answer = True
+            action = getattr(self, self.choices["any"])
+            action()
+        else:
+            for choice in self.choices:
+                if answer == choice:
+                    valid_answer = True
+                    action = getattr(self, self.choices[answer])
+                    action()
+                    break
+        if not valid_answer:
+            self.game.screen_get('menu').add(
+                '\nPlease choose a valid option.', True, True)
+            self.userchoice()
+
+    def newgame(self):
+        self.game.menu_list[1].start()
+
+    def loadgame(self):
+        pass
+
+    def exit(self):
+        self.game.exit()
 
 
 class Screen:
@@ -64,40 +124,6 @@ class Screen:
             if not line[1]:
                 new_strlist.append(line)
         self.strlist = new_strlist
-
-
-class Menu:
-
-    def __init__(self, game, menufile_path):
-        self.game = game
-        self.menufile = ReaderTXT(menufile_path)
-        self.read_menu(0)
-        self.userchoice()
-
-    def read_menu(self, section):
-        self.game.screen_reset('menu')
-        for line in self.menufile.get_section(section):
-            self.game.screen_get('menu').add(line)
-        self.game.screen_refresh('menu')
-
-    def userchoice(self):
-        choice = self.game.question('menu')
-        if choice == "1":  # Start a game
-            self.newgame()
-        elif choice == "2":  # Load a save
-            self.loadgame()
-        elif choice == "3":  # Exit
-            self.game.exit(self.userchoice)
-        else:
-            self.game.screens['menu'].add('\nPlease choose a valid option.',
-                                          True, True)
-            self.userchoice()
-
-    def newgame(self):
-        pass
-
-    def loadgame(self):
-        pass
 
 
 class ReaderTXT:
