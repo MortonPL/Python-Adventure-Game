41dccd907f0a4864906f0e2b83c614b13cfa808c
MortonPL <MortonPL@users.noreply.github.com>
2019-12-30 20:45:30 +0100

    Unstable version with data module for variables

---
+++ data.py
@@ -0,0 +1,14 @@
+# This is the data module that holds all globals
+path = None
+platform = None
+list_of_screens = {}
+current_screen = None
+rollback = None
+rollback_args = ()
+list_of_system_calls = {}
+list_of_calls = {}
+list_of_locations = []
+list_of_menus = []
+playable_characters = {}
+player = None
+version = "Game_A"
